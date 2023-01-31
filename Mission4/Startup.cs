385a40deb38d31895b76e4b877ebe2f913608067
YuiZoldyck <betasan28@gmail.com>
2023-01-30 23:06:17 -0700

    Finished up code and add comments

--- Mission4/Startup.cs
+++ Mission4/Startup.cs
@@ -21,20 +21,22 @@ namespace Mission4
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
 
-            app.UseRouting();
+            //app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
+
+            app.UseStaticFiles();
         }
     }
 }
---
+++ Mission4/wwwroot/js/gradeCalc.js
@@ -0,0 +1,64 @@
+﻿//function to calculate the grade
+
+$("#btnCalc").click(function () {
+
+    //take in all the variables/scores
+
+    var assignmentgrade = $("#assignments").val();
+    var groupgrade = $("#groupProject").val();
+    var quizgrade = $("#quizzes").val();
+    var midgrade = $("#midterm").val();
+    var fingrade = $("#final").val();
+    var intexgrade = $("#intex").val();
+
+    //calclation to figure out what the grade average is
+
+    finalgrade = assignmentgrade * .5 + groupgrade * .1 + quizgrade * .1 + midgrade * .1 + fingrade * .1 + intexgrade * .1;
+    console.log(finalgrade);
+
+    let gradeLetter = "";
+
+    //switch statment that takes the final grade and finds the corresponding letter grade
+
+    switch (true) {
+        case finalgrade >= 94:
+            gradeLetter = "A";
+            break;
+        case finalgrade >= 90:
+            gradeLetter = "A-";
+            break;
+        case finalgrade >= 87:
+            gradeLetter = "B+";
+            break;
+        case finalgrade >= 84:
+            gradeLetter = "B";
+            break;
+        case finalgrade >= 80:
+            gradeLetter = "B-";
+            break;
+        case finalgrade >= 77:
+            gradeLetter = "C+";
+            break;
+        case finalgrade >= 74:
+            gradeLetter = "C";
+            break;
+        case finalgrade >= 70:
+            gradeLetter = "C-";
+            break;
+        case finalgrade >= 67:
+            gradeLetter = "D+";
+            break;
+        case finalgrade >= 64:
+            gradeLetter = "D";
+            break;
+        case finalgrade >= 60:
+            gradeLetter = "D-";
+            break;
+        case finalgrade < 60:
+            gradeLetter = "E";
+            break;
+    }
+
+    //replaces input in input box with id letter with the gradeletter
+    $("#letter").val(gradeLetter);
+})
